a5f84045ae11fd6c88e848fa254bf88be2073e9e
TOGLAN <93621149+lionelmarcus10@users.noreply.github.com>
2022-05-24 07:55:53 +0200

    Update ScreenConst.py

--- Const/ScreenConst.py
+++ Const/ScreenConst.py
@@ -6,24 +6,32 @@ levels = [
                 'init_pos': (200, 460),
                 'background': "Assets/level1.png",
                 'border': "Assets/level1_border.png",
-                'hole': "Assets/level1_hole.png"
+                'hole': "Assets/level1_hole.png",
+                'hole_pos': (330, 230),
+                'bsize': 10,
             },
             {
-                'init_pos': (200, 460),
+                'init_pos': (118, 267),
                 'background': "Assets/level2.png",
                 'border': "Assets/level2_border.png",
-                'hole': "Assets/level1_hole.png"
+                'hole': "Assets/level2_hole.png",
+                'hole_pos': (511, 340),
+                'bsize': 7,
             },
             {
-                'init_pos': (0, 0),
+                'init_pos': (318, 417),
                 'background': "Assets/level3.png",
                 'border': "Assets/level3_border.png",
-                'hole': "Assets/level3_hole.png"
+                'hole': "Assets/level3_hole.png",
+                'hole_pos': (452, 170),
+                'bsize': 10,
             },
             {
                 'init_pos': (480, 140),
                 'background': "Assets/level4.png",
                 'border': "Assets/level4_border.png",
-                'hole': "Assets/level4_hole.png"
+                'hole': "Assets/level4_hole.png",
+                'hole_pos': (330, 230),
+                'bsize': 10,
             }
         ]
